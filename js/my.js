f60aaf6c9fd2c586a55bb7b8144bbb69ee775b3d
jfomay <jfomay@addu.edu.ph>
2021-03-10 16:12:58 +0800

    initial commit

--- js/my.js
+++ js/my.js
@@ -1,15 +1,98 @@
-let array = ["aaa","bbb","ccc","ddd"];
-let index = 0;
-let names = "";
-while(array[index]){
-   names += array[index] + " ";
-    index++;
-}
-console.log(names);
+var days = ["Sunday","Monday","Tuesday","Wensday","Thursday","Friday","Saturday"]
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
-const test3 = () => console.log("Hello World");
-test3();
+ var tableBody = document.getElementById("body");
+ var month = parseInt(prompt("Enter Month 1-12: "));
+ var day = parseInt(prompt("Enter Date (1-7): "));
 
-const addTwo = (n1 = 0,n2 = 0) => n1 + n2;
-const sum = addTwo(1,2);
-console.log(sum);
+ var end = true
+ var currentDay = 1
+ var checker = 0;
+
+ if(month < 13 && day < 8 && month > 0 && day > 0){
+     var title = document.getElementById("title");
+     var titleTextNode = document.createTextNode(months [month -1]);
+     title.appendChild(titleTextNode);
+     while(end){
+         var tr = document.createElement("tr");
+         for(var i = 0; i < days.length; i++){
+             if ( i == day - 1){
+                 if (checker == 1){
+                     var td = document.createElement("td");
+                     var tdTextNode = document.createTextNode(currentDay);
+                     td.appendChild(tdTextNode);
+                     tr.appendChild(td);
+                     tableBody.appendChild(tr);
+                     if(i==0){
+                         td.className = "body-color";
+                     }
+                     currentDay++;
+                 }
+                 else{
+                     var td = document.createElement("td");
+                     var tdTextNode = document.createTextNode(currentDay);
+                     td.appendChild(tdTextNode);
+                     tr.appendChild(td);
+                     tableBody.appendChild(tr);
+                     if( i == 0){
+                         td.className = "body-color";
+                     }
+                     currentDay++;
+                     checker = 1;
+                 }
+             }
+             else{
+                 if(checker == 1){
+                     if(currentDay >= 32 && (month == 1 || month == 3 ||month == 5 ||month == 7 ||month == 8 ||month == 10 ||month == 12)){
+                         end = false;
+                         break;
+                     }
+                     else if(currentDay >= 31 && (month == 4 || month == 6 ||month == 9 ||month == 11 )){
+                        end = false;
+                        break;
+                    }
+                    else if(currentDay >= 28 && (month == 2)){
+                        var td = document.createElement("td");
+                        var tdTextNode = document.createTextNode('28');
+
+                        td.appendChild(tdTextNode);
+                        tr.appendChild(td);
+                        tableBody.appendChild(tr);
+                        if(i==0){
+                            td.className = "body-color";
+                        }
+                        end = false;
+                        break;
+                    }
+                    else{
+                        var td = document.createElement("td");
+                        var tdTextNode = document.createTextNode(currentDay);
+
+                        td.appendChild(tdTextNode);
+                        tr.appendChild(td);
+                        tableBody.appendChild(tr);
+                        if( i == 0){
+                            td.className = "body-color";
+                        }
+                        currentDay++;
+                    }
+                 }
+                 else{
+                     var td = document.createElement("td");
+                     var tdTextNode = document.createTextNode('');
+                     td.appendChild(tdTextNode);
+                     tr.appendChild(td);
+                     tableBody.appendChild(tr);
+                     if (i == 0){
+                         td.className = "body-color";
+                     }
+                 }
+             }
+         }
+     }
+ }
+ else{
+     var hone = document.getElementById("error");
+     var honeTextNode = document.createTextNode("Invalid Month or Day!!");
+     hone.appendChild(honeTextNode);
+ }
